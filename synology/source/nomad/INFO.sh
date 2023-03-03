bdb8b9870a690fab1e2276f1933989bf824e09d3
Paul Gokke <paul.gokke@sma-services.nl>
2023-03-03 16:43:07 +0100

    Added the Synology sources to be able to generate a Synology package using the Synology build toolkit.

---
+++ synology/source/nomad/INFO.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Copyright (c) 2023 ATVANZT All rights reserved.
+
+TIMESTAMP=$(date -u +%Y%m%d-%H:%M:%S)
+
+os_min_ver="7.0-40356"
+os_max_ver=""
+
+source /pkgscripts/include/pkg_util.sh
+
+package="nomad"
+version="{put_synology_package_version_here}"
+displayname="HashiCorp Nomad"
+maintainer=ATVANZT"
+maintainer_url="https://www.atvanzt.nl"
+arch="noarch"
+description="Orchestration Made Easy by HashiCorp Nomad."
+create_time="${TIMESTAMP}"
+support_conf_folder="yes"
+os_min_ver="${os_min_ver}"
+os_max_ver="${os_max_ver}"
+silent_install="no"
+silent_uninstall="no"
+silent_upgrade="no"
+[ "$(caller)" != "0 NULL" ] && return 0
+pkg_dump_info
