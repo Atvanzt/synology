f246ecf7782fb30f6301d67b4849f8c6addfd78c
Paul Gokke <paul.gokke@sma-services.nl>
2023-05-10 12:05:40 +0200

    While log directory is not created automatically, we first will use a valid location without any hussle.

--- synology/source/vault/package/start.sh
+++ synology/source/vault/package/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 source /var/packages/vault/var/env.sh
-/var/packages/vault/target/package/bin/vault server -config ${VAULT_SHARE_DIR}/etc/vault.d/vault.hcl --log-level="info" > $(VAULT_SHARE_DIR}/log/vault.log
+/var/packages/vault/target/package/bin/vault server -config ${VAULT_SHARE_DIR}/etc/vault.d/vault.hcl --log-level="info" > ${VAULT_SHARE_DIR}/vault.log
